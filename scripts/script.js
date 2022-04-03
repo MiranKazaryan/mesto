d7be652a146b7a1bd991b24d0602ccdef1a24d97
Miran Kazaryan <mir12344@mail.ru>
2022-04-03 14:13:51 +0300

    исправления первой итерации

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,30 @@
-let popup=document.querySelector('.popup');
-
-let editButton=document.querySelector('.profile__edit-button');
-let saveButton=document.querySelector('.popup__saveButton');
-let closeButton=document.querySelector('.popup__closeButton');
-
-let profileName=document.querySelector('.profile__title');
-let profileDescription=document.querySelector('.profile__subtitle');
-
-let inputName=document.querySelector('.popup__input_type_name');
-let inputDescription=document.querySelector('.popup__input_type_description');
-
-let form=document.querySelector('.popup__form');
-
-//открываем окно редактирования профиля
-editButton.addEventListener('click',function()
-{
-    //popup.classList.remove('popup');
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const saveButton = document.querySelector('.popup__save-button');
+const closeButton = document.querySelector('.popup__close-button');
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__subtitle');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputDescription = document.querySelector('.popup__input_type_description');
+const form = document.querySelector('.popup__form');
+//функция редактирования
+function Edit(){
     popup.classList.add('popup_opened');
     inputName.value=profileName.textContent;
     inputDescription.value=profileDescription.textContent;
-})
-
-
-closeButton.addEventListener('click',function(){
+}
+//функция закрытия
+function Close(){
     popup.classList.remove('popup_opened');
-   // popup.classList.add('popup');
-})
-document.addEventListener('keydown', function(event){
-	if(event.key === "Escape"){
-        popup.classList.remove('popup_opened');
-       // popup.classList.add('popup');
-	}
-});
-
-
-
+}
+//функция изменения данных
 function changeUser(evt){
     evt.preventDefault();
     console.log('Форма отправлена');
     profileName.textContent=inputName.value;
     profileDescription.textContent=inputDescription.value;
-    popup.classList.remove('popup_opened');
-    popup.classList.add('popup');
+    Close();
 }
-document.addEventListener('keydown', function(event){
-	if(event.key === "Enter"){
-        form.addEventListener('submit',changeUser);
-	}
-});
-
+editButton.addEventListener('click',Edit);
+closeButton.addEventListener('click',Close);
 form.addEventListener('submit',changeUser);
